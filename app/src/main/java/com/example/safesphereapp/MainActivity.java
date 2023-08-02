8584afd63517c0b93c89b08e7eb23ae15bcf256e
Rahil Patel <rahilp0920@gmail.com>
2023-08-02 15:12:07 -0400

    SignUpPageFirstPart

--- app/src/main/java/com/example/safesphereapp/MainActivity.java
+++ app/src/main/java/com/example/safesphereapp/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.safesphereapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.TextView;
+
+
 
 public class MainActivity extends AppCompatActivity {
     EditText username;
     EditText password;
     Button loginButton;
+
+    TextView signUpButton;
+    TextView forgotYourPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
         username = findViewById(R.id.usernameID);
         password = findViewById(R.id.passwordID);
         loginButton = findViewById(R.id.loginButton);
+        signUpButton = findViewById(R.id.signUpButton);
+        forgotYourPassword = findViewById(R.id.forgotYourPassword);
+
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,6 +38,22 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     Toast.makeText(MainActivity.this, "Login Failed!", Toast.LENGTH_SHORT).show();
                 }
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signUpSwitch = new Intent(MainActivity.this, SignUpActivity.class);
+                MainActivity.this.startActivity(signUpSwitch);
+            }
+        });
+
+        forgotYourPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent forgotYourPasswordSwitch = new Intent(MainActivity.this, forgotYourPassword.class);
+                MainActivity.this.startActivity(forgotYourPasswordSwitch);
             }
         });
     }
